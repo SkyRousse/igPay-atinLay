2f6a0f8fd9a895e653bd3d168bc3106585fdb064
Marty Fitchen and Maldon Meehan <maldon@>
2016-06-08 15:41:30 -0700

    add code for consonants, it is shifting the first letter, but currently adding the word and ay several times

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 // JavaScript/jQuery Business Logic
 var pigArray = [];
+// var vowel = ["a", "e", "i", "o", "u"];
 function pigLatin(englishArray) {
   if(englishArray[0] === "a" || englishArray[0] === "e" || englishArray[0] === "i" || englishArray[0] === "o" || englishArray[0] === "u") {
   pigArray = pigArray.concat(englishArray);
   pigArray.push("a", "y");
-  }
 
-  return pigArray.join("");
-  console.log(pigArray);
+} else {
+    for(var index = 0; index < englishArray.length; index += 1) {
+      englishArray.push(englishArray.shift());
+      pigArray = pigArray.concat(englishArray);
+      pigArray.push("a", "y");
+    }
+  }
+return pigArray.join("");
+console.log(pigArray);
 };
 
 // JavaScript/jQuery Front-End Logic
