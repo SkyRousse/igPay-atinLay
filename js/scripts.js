d900fefd95f24379a8e42ebdd4b14c1547e9aa6c
Marty Fitchen and Maldon Meehan <marty@>
2016-06-08 16:58:01 -0700

    add working vowel and consonants, we solved by having push and concat outside of our if statement, we be happy now

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,29 @@ var pigArray = [];
 var vowel = ["a", "e", "i", "o", "u"];
 function pigLatin(englishArray) {
   if(englishArray[0] === "a" || englishArray[0] === "e" || englishArray[0] === "i" || englishArray[0] === "o" || englishArray[0] === "u") {
-  pigArray = pigArray.concat(englishArray);
-  pigArray.push("a", "y");
+  // pigArray = pigArray.concat(englishArray);
+  // // pigArray.push("a", "y");
 
 } else {
-    for(var index = 0; index < englishArray.length; index += 1) {
-      if (englishArray[index] !== vowel[0] || englishArray[index] !== vowel[1] || englishArray[index] !== vowel[2] || englishArray[index] !== vowel[3] || englishArray[index] !== vowel[4]) {
-        pigArray.push(pigArray.shift());
-        pigArray = pigArray.concat(englishArray);
-        pigArray.push("a", "y");
+  var secondVowel = false;
+    for(var index = 0; index < englishArray.length; index += 0) {
+      if (englishArray[index] !== vowel[0] && englishArray[index] !== vowel[1] && englishArray[index] !== vowel[2] && englishArray[index] !== vowel[3] && englishArray[index] !== vowel[4]) {
+        debugger;
+        pigArray.push(englishArray.shift());
       }
+      else{
+        secondVowel = true;
+      }
+
+      if (secondVowel){
+        break;
+
+      }
+      // pigArray = englishArray.concat(pigArray);
     }
   }
+  pigArray.push("a", "y");
+  pigArray = englishArray.concat(pigArray);
 return pigArray.join("");
 console.log(pigArray);
 };
