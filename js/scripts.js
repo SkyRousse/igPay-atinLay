24b1848549723654fa5c61603a3a9bd8ad6037e9
sr <skyrousse@gmail.com>
2016-06-09 00:33:09 -0700

    add array to slice based on spaces and logic to test for bad characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,43 @@
 // JavaScript/jQuery Business Logic
-var pigArray = [];
 var vowel = ["a", "e", "i", "o", "u"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+var illegalCharacters = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "|", "\\", "/", "~", "[", "]", "{", "}"];
+var allowedPunctuation = [".", "?", "!", ","];
+
+var illegalInput = false;
+var multiWord = false;
+var stringsArray = [];
+
+
+
+
 function pigLatin(englishArray) {
+  debugger;
+  englishArray = englishArray.toString().split("");
+  for (var index = 0; index < englishArray.length; index++) {
+    if (illegalCharacters.includes(englishArray[index])) {
+      illegalInput = true;
+    }
+    else if (englishArray[index] === " ") {
+      multiWord = true;
+    }
+  }
+  if (illegalInput) {
+    alert("bad input");
+  }
+  else if (multiWord) {
+    stringsArray = englishArray.split(" ");
+    var pigArray = [];
+    pigArray = stringsArray.map(function(word) {
+      return pigTranslator(word);
+    });
+    var result = pigArray.join(' ');
+    return result;
+  }
+  else {
+    // return pigTranslator(string);
+  }
+
   if(englishArray[0] === "a" || englishArray[0] === "e" || englishArray[0] === "i" || englishArray[0] === "o" || englishArray[0] === "u") {
   // pigArray = pigArray.concat(englishArray);
   // // pigArray.push("a", "y");
@@ -30,6 +66,41 @@ return pigArray.join("");
 console.log(pigArray);
 };
 
+
+
+// begin JMDelight's code
+var inputTranslator = function(input) {
+  illegalInput = false;
+  multiWord = false;
+  for (i = 0; i < string.length; i++) {
+    if (illegalCharacters.includes(string[index])) {
+      illegalInput = true;
+    }
+    else if (string[index] === " ") {
+      multiWord = true;
+    }
+    //may want to refactor to have a different default case? else?
+  }
+  if (illegalInput) {
+    alert("bad input");
+  }
+  else if (multiWord) {
+    stringsArray = string.split(" ");
+    var pigArray = [];
+    pigArray = stringsArray.map(function(word) {
+      return pigTranslator(word);
+    });
+    var result = pigArray.join(' ');
+    return result;
+  }
+  else {
+    return pigTranslator(string);
+  }
+};
+//end JMDelight's code
+
+
+
 // JavaScript/jQuery Front-End Logic
 $(document).ready(function() {
   $("form#pig-latin-form").submit(function(event) {
@@ -37,7 +108,7 @@ $(document).ready(function() {
 
     var englishString = $("input#english").val();
     var englishLowerCase = englishString.toLowerCase();
-    var englishArray = englishLowerCase.split("");
+    var englishArray = englishLowerCase.split(" ");
     pigArray = pigLatin(englishArray)
 
     $(".translation").text(pigArray);
